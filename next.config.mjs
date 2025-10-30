c80ee25b33e7cb31fd05414eb89be079d38aeff6
Nick Kolodziej <nick@koloquial.cc>
2025-10-30 13:29:11 -0500

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: { unoptimized: true }, // optional but helpful on Render
 };
 
-export default nextConfig;
+module.exports = nextConfig;
